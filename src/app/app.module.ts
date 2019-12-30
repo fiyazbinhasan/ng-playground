57960d4e29ec23afcaf8733ee9803332fc8ac237
Fiyaz Hasan <fiyazhasan@live.com>
2019-12-30 15:05:06 +0600

    yes or no pipe added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { UserComponent } from "./user/user.component";
+import { YesNoPipe } from "./shared/yes-no.pipe";
 
 @NgModule({
-  declarations: [AppComponent, UserComponent],
+  declarations: [AppComponent, UserComponent, YesNoPipe],
   imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,30 +12,60 @@ import {
   styleUrls: ["./user.component.scss"]
 })
 export class UserComponent implements OnInit {
-  userForm: FormGroup;
-
-  get name() {
-    return this.userForm.get("name");
-  }
-
-  get email() {
-    return this.userForm.get("email");
-  }
-
-  constructor(private fb: FormBuilder) {}
+  structure: any;
+  data: Array<any>;
 
   ngOnInit() {
-    this.createForm();
-  }
+    this.structure = {
+      team: "string",
+      played: "number",
+      won: "number",
+      lost: "number",
+      points: "number",
+      isQualified: "boolean"
+    };
 
-  createForm() {
-    this.userForm = this.fb.group({
-      name: ["", [Validators.required]],
-      email: ["", [Validators.required, Validators.email]]
-    });
-  }
-
-  onSubmit() {
-    console.log(this.userForm.value);
+    this.data = [
+      {
+        team: "AUS",
+        played: 9,
+        won: 7,
+        lost: 2,
+        points: 14,
+        isQualified: true
+      },
+      {
+        team: "ENG",
+        played: 9,
+        won: 6,
+        lost: 3,
+        points: 12,
+        isQualified: true
+      },
+      {
+        team: "NZ",
+        played: 9,
+        won: 5,
+        lost: 3,
+        points: 11,
+        isQualified: true
+      },
+      {
+        team: "SA",
+        played: 9,
+        won: 3,
+        lost: 5,
+        points: 7,
+        isQualified: false
+      },
+      {
+        team: "WI",
+        played: 9,
+        won: 2,
+        lost: 6,
+        points: 5,
+        isQualified: false
+      }
+    ];
   }
 }
